7f511334543002aa59e6307b4e29cef606f5dd96
Jameka Echols <jameka.echols@gmail.com>
2021-03-14 21:09:37 -0500

    added outlets and actions of the login page to controller

--- instagram/LoginViewController.swift
+++ instagram/LoginViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +18,9 @@ class LoginViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signIn(_ sender: Any) {
     }
-    */
+    @IBAction func signUp(_ sender: Any) {
+    }
 
 }
