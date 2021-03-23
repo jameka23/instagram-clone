bdc2a3db2fe6bc06202f08eadab7b57a940b17da
Jameka Echols <jameka.echols@gmail.com>
2021-03-22 22:42:21 -0500

    added persist gif

--- instagram/SceneDelegate.swift
+++ instagram/SceneDelegate.swift
@@ -20,6 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
+        // for persisten login of current user
         if PFUser.current() != nil{
             let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
             self.window = UIWindow(windowScene: scene as! UIWindowScene)
